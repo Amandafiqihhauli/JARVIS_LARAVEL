5ac078d238a0d27ec36caf9b06af4a966ebff3d1
Amandafiqihhauli <piki.cou@gmail.com>
2024-05-25 21:08:06 +0700

    tugas 6

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perkenalan Diri</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background: linear-gradient(to right, #ebc6db, #e45a7d);
+            color: #aa005e;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 5;
+        }
+        .container {
+            background: rgba(240, 233, 233, 0.6);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            text-align: center;
+        }
+        h1 {
+            margin: 0;
+            font-size: 3em;
+        }
+        p {
+            font-size: 1.2em;
+        }
+        .social-icons {
+            list-style: none;
+            padding: 0;
+            margin-top: 20px;
+            display: flex;
+            justify-content: center;
+        }
+        .social-icons li {
+            margin: 0 10px;
+        }
+        .social-icons a {
+            color: #9c0068;
+            font-size: 2em;
+            text-decoration: none;
+        }
+        .social-icons a:hover {
+            color: #c05493;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Halo, Nama Saya Amanda Fiqih Hauli</h1>
+        <p>Selamat datang di halaman perkenalan Manda. Saya seorang Mahasiswa STT Terpadu Nurul Fikri. Saya memiliki minat besar dalam dunia teknologi yaitu Data Analis. Saya sangat antusias untuk mempelajari berbagai pengetahuan dan pengalaman baru. Saya sedang mempelajari ilmu pemrograman web bersama Jarviscamp</p>
+        <h4>Kontak</h4>
+        <ul class="social-icons">
+            <li><a href="https://www.linkedin.com/in/amanda-fiqih-90b87729b/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://github.com/amandafiqihhauli" target="_blank"><i class="fab fa-github"></i></a></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/perkenalan', function () {
+    return view('perkenalan');
+});
+
+
